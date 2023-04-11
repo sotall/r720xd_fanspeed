5b38a824dad934788151f9e86c0478caea34b1bf
sotall <lorenmacneil@gmail.com>
2023-04-11 10:38:16 -0700

    fix script.sh file

--- script.sh
+++ script.sh
@@ -10,53 +10,61 @@ usr=$2
 pw=$3
 echo "iDRAC Username: $usr"
 
-# check for nVidia driver
-if command -v nvidia-smi &> /dev/null; then
-  gpu_temp=$(nvidia-smi --query-gpu="temperature.gpu" --format=csv,noheader)
-  echo -e "\nNvidia drivers are installed\nI can see these Nvidia gpus in your server\n"
-  nvidia-smi --list-gpus 
+if command -v nvidia-smi &>/dev/null; then
+    echo -e "\nNvidia drivers are installed\nI can see these Nvidia GPUs in your server\n"
+    nvidia-smi --list-gpus
 
-  # set persistence mode for gpus
-  echo "Checking if persistence mode is enabled"
-  if [[ $(nvidia-smi --query-gpu="persistence_mode" --format=csv,noheader) = "Disabled" ]]; then
-    echo "Enabling persistence mode"
-    nvidia-smi --persistence-mode=1
-  else
-    echo "Persistence mode is already enabled"
-  fi
+    if [[ $(nvidia-smi --query-gpu="persistence_mode" --format=csv,noheader) == "Disabled" ]]; then
+        nvidia-smi --persistence-mode=1
+    fi
 
+    gpu_temp=$(nvidia-smi --query-gpu="temperature.gpu" --format=csv,noheader) || gpu_temp=0
 else
-  gpu_temp=0 # Set GPU temp to 0 if NVIDIA driver is not installed
+    gpu_temp=0
 fi
 
-# query temperatures
-temp=$(ipmitool -I lanplus -H $iDRAC -U $usr -P $pw sdr type temperature)
-cpu1_temp=$(echo "$temp" | grep 0Eh | cut -d \| -f5 | grep -Po '\d\d')
-cpu2_temp=$(echo "$temp" | grep 0Fh | cut -d \| -f5 | grep -Po '\d\d')
-exhaust_temp=$(echo "$temp" | grep 01h | cut -d \| -f5 | grep -Po '\d\d')
-gpu_temp=$(nvidia-smi --query-gpu="temperature.gpu" --format=csv,noheader)
+highest_temp=$gpu_temp
+while read -r line; do
+    temp=$(echo "$line" | grep -oE '[[:digit:]]+ degrees C$' | cut -d' ' -f1)
+    ((temp > highest_temp)) && highest_temp=$temp
+done < <(ipmitool -I lanplus -H "$iDRAC" -U "$usr" -P "$pw" sdr type temperature | grep -oE '[[:digit:]]{1,2}h')
 
-echo "CPU1 Temp: $cpu1_temp"
-echo "CPU2 Temp: $cpu2_temp"
-echo "Exhaust Temp: $exhaust_temp"
-echo "GPU Temp: $gpu_temp"
+echo -e "\nHighest temp: $highest_temp"
 
-# get highest cpu and gpu temps
-cpu_temp=$((cpu1_temp > cpu2_temp ? cpu1_temp : cpu2_temp))
-echo "User highest CPU temp"
-echo "CPU Temp: $cpu_temp"
-
-gpu_temp=$(nvidia-smi --query-gpu="temperature.gpu" --format=csv,noheader)
-echo "GPU Temp: $gpu_temp"
-
-# set fan speed based on highest temperature
 declare -A fan_speeds=(
-    [100]=19200 [80]=15360 [70]=13440 [60]=11520 [50]=9600 [45]=8760 [40]=8040 [35]=7200 [30]=6480 [25]=5640 [20]=4800 [15]=4080 [10]=3240 [5]=2400
-    )
-fan_speed=$(for temp in "${!fan_speeds[@]}"; do 
-  [[ $cpu_temp -ge $temp || $gpu_temp -ge $temp ]] && echo "${fan_speeds[$temp]}" && break 
-done)
-echo "Setting fan speed to ${fan_speed:-0} RPM"
+    [100]=0x64  # 19200 RPM
+    [80]=0x50  # 15360 RPM
+    [70]=0x46  # 13440 RPM
+    [60]=0x3c  # 11520 RPM
+    [50]=0x32  # 9600 RPM
+    [45]=0x2d  # 8760 RPM
+    [40]=0x28  # 8040 RPM
+    [35]=0x23  # 7200 RPM
+    [30]=0x1e  # 6480 RPM
+    [25]=0x19  # 5640 RPM
+    [20]=0x14  # 4800 RPM
+    [15]=0x0f  # 4080 RPM
+    [10]=0x0a  # 3240 RPM
+    [5]=0x05  # 2400 RPM
+    [0]=0x00  # 1800 RPM 
+)
+for temp in $(echo "${!fan_speeds[@]}" | tr ' ' '\n' | sort -n); do
+    dec_value=$(printf "%d" ${fan_speeds[$temp]})
+    rpm=$(echo "172.8 * $dec_value + 1800 + 0.5" | bc)
+    rpm=$(printf "%.0f" $rpm)
+    last_percent=$fan_percent
 
-# set the fan speed using IPMItool
-ipmitool -I lanplus -H $iDRAC -U $usr -P $pw raw 0x30 0x30 0x02 0xff "${fan_speed:-0}"
+    if (($highest_temp >= $temp)); then
+        echo -e "\nTemp: $temp"
+        echo "Fan speed: ${fan_speeds[$temp]}"
+        echo "Highest temp: $highest_temp"
+        echo "${temp}% - ${rpm} RPM"
+        fan_percent=${fan_speeds[$temp]}
+        last_rpm=$rpm
+    else
+        break
+    fi
+done
+
+echo -e "\nSetting fan speed to around ${last_rpm} RPM"
+ipmitool -I lanplus -H "$iDRAC" -U "$usr" -P "$pw" raw 0x30 0x30 0x02 0xff "${fan_percent:-0x19}"
